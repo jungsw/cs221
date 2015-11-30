f34a6ab7dea47a8076028bdb66020628c4fe1aac
Chan Lee <chanlee@DN0a229055.SUNet>
2015-11-30 00:12:56 -0800

    About half of input file processed for lat, long

--- locationCrawler.py
+++ locationCrawler.py
@@ -2,17 +2,19 @@ import urllib2
 import urllib
 import json
 
-# https://maps.googleapis.com/maps/api/geocode/json?address=04TH%20ST%20and%20PERRY%20ST%20in%20san%20francisco
-apiKey = 'AIzaSyCO7AAKGTSz9p-ZfIvPeqyPDtMAQA27jZ0'
+# https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDX1GshmuCV3qfFCfoBqHTtVw4SFbLWT8w&address=04TH%20ST%20and%20PERRY%20ST%20in%20san%20francisco
+# apiKey = 'AIzaSyCO7AAKGTSz9p-ZfIvPeqyPDtMAQA27jZ0'   # versiempre@gmail.com
+# apiKey = 'AIzaSyCufQQEadq3JZOx5sXfwpfy4AUcR1AIXMM'   # sw
+# apiKey = 'AIzaSyDqDhnuWqA6KWy8-VM_tTByD4mgFWIP0pM'   # hanklee514@gmail.com
+apiKey = 'AIzaSyDX1GshmuCV3qfFCfoBqHTtVw4SFbLWT8w'   # jake
 baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json'
 
-# inputFile = open('List_of_Intersections_only.csv', 'r')
-inputFile = open('sample_data.csv', 'r')
-outputFile = open('sample_output.csv', 'w')
+inputFile = open('IntersectionsWithLatLng.csv', 'r')
+outputFile = open('IntersectionsWithLatLng_v2.csv', 'w')
+startIndex = 7391
 
-# key: set([street1, street2]), value: {lat, lng, name}
+# Prevent querying duplicate
 cache = {}
-
 def generateCacheKey(street1, street2):
 	keyList = [street1, street2]
 	keyList.sort()
@@ -20,45 +22,52 @@ def generateCacheKey(street1, street2):
 
 succCount, cacheCount, failedCount = 0,0,0
 
-for line in inputFile.readlines():
-	streets = line.split(',')
-	lat, lng, name = 0, 0, ''
 
-	cKey = generateCacheKey(streets[1], streets[2])
-	if cKey in cache:
-		result = cache[cKey]
-		lat, lng, name = result['lat'], result['lng'], result['name']
-		cacheCount += 1
-		print 'cache'
-	else:
-		query = {'address': '%s and %s in San Francisco' %(streets[1], streets[2]), \
-						 'key': apiKey }
-		url = baseUrl + '?' + urllib.urlencode(query)
+for index, line in enumerate(inputFile.readlines()):
+	if index >= startIndex and line[0]!='*' \
+		and 'END,,,,END' not in line:
 
-		try:
-			response = urllib2.urlopen(url)
-		except urllib2.HTTPError, err:
-			print 'Http error %s: %s' %(err.code, err.read())
-		except urllib2.URLError, err:
-			print 'Url error %s: %s' %(err.code, err.read())
+		streets = line.split(',')
+		st1 = streets[1]
+		st2 = streets[2].split('\\')[0]
+		lat, lng, name = 0, 0, ''
+
+		cKey = generateCacheKey(st1, st2)
+		if cKey in cache:
+			result = cache[cKey]
+			lat, lng, name = result['lat'], result['lng'], result['name']
+			cacheCount += 1
+			print 'cache'
 		else:
-			results = json.load(response)['results'][0]
-			if results['types'][0]=='intersection':
-				loc = results['geometry']['location']
-				lat, lng, name = loc['lat'], loc['lng'], results['formatted_address']
-				cache[cKey] = {'lat': lat, 'lng': lng, 'name': name}
-				 
-				succCount += 1
-				print 'success'
+			query = {'address': '%s and %s in San Francisco' %(st1, st2), \
+							 'key': apiKey }
+			url = baseUrl + '?' + urllib.urlencode(query)
+			print url
+
+			try:
+				response = urllib2.urlopen(url)
+			except urllib2.HTTPError, err:
+				print 'Http error %s: %s' %(err.code, err.read())
+			except urllib2.URLError, err:
+				print 'Url error %s: %s' %(err.code, err.read())
+			else:
+				results = json.load(response)['results'][0]
+				if results['types'][0]=='intersection':
+					loc = results['geometry']['location']
+					lat, lng, name = loc['lat'], loc['lng'], results['formatted_address']
+					cache[cKey] = {'lat': lat, 'lng': lng, 'name': name}
+					 
+					succCount += 1
+					print 'success'
 	
-	# For each line in output file, 
-	# search success -> '* lat, long | formatted_address | original input'
-	# failure -> 'original input'
-	if lat > 0:
-		line = '* %f, %f | %s | ' %(loc['lat'], loc['lng'], results['formatted_address']) + line
-	else:
-		failedCount += 1
-		print 'failure'
+		# For each line in output file, 
+		# search success -> '* lat, long | formatted_address | original input'
+		# failure -> 'original input'
+		if lat > 0:
+			line = '* %f, %f | %s | ' %(loc['lat'], loc['lng'], results['formatted_address']) + line
+		else:
+			failedCount += 1
+			print 'failure'
 
 	outputFile.write(line)
 
