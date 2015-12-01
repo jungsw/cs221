8965286215a9219facde8665de9166204816d633
Chan Lee <chanlee@DN0a22605c.SUNet>
2015-12-01 00:48:40 -0800

    Finished initial crawling of lat lng data for intersections

--- locationCrawler.py
+++ locationCrawler.py
@@ -7,33 +7,39 @@ import json
 # versiempre@gmail.com, hanklee514@gmail.com, jake, sw
 apiKeyList = ['AIzaSyCO7AAKGTSz9p-ZfIvPeqyPDtMAQA27jZ0', 'AIzaSyDqDhnuWqA6KWy8-VM_tTByD4mgFWIP0pM', \
 							'AIzaSyDX1GshmuCV3qfFCfoBqHTtVw4SFbLWT8w', 'AIzaSyCufQQEadq3JZOx5sXfwpfy4AUcR1AIXMM']
-apiKey = apiKeyList[3]
+apiKey = apiKeyList[2]
 baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json'
 
 inputFile = open('IntersectionsWithLatLng.csv', 'r')
-startIndex = 7391
+startIndex = 17195
 
+# Prevent querying duplicate
+cache = {}
+def generateCacheKey(street1, street2):
+	keyList = [street1, street2]
+	keyList.sort()
+	return ' '.join(keyList)
 
 def crawl():
 	outputFile = open('IntersectionsWithLatLng_v2.csv', 'w')
-	# Prevent querying duplicate
-	cache = {}
-	def generateCacheKey(street1, street2):
-		keyList = [street1, street2]
-		keyList.sort()
-		return ' '.join(keyList)
-
-
+	
 	for index, line in enumerate(inputFile.readlines()):
-		if index >= startIndex and line[0]!='*' \
-			and 'END,,,,END' not in line:
+		streets = line.split('|')[-1].split(',')
+		st1 = streets[1]
+		st2 = streets[2].split('\\')[0]
+		cKey = generateCacheKey(st1, st2)
+		
+		# Fill Cache
+		if index < startIndex and line[0]=='*':
+			if cKey not in cache:
+				latLng = line.split('|')[0].replace('*', '').split(',')
+				lat, lng = map(lambda x: float(x), latLng)
+				name = line.split('|')[1].strip()
+				cache[cKey] = {'lat': lat, 'lng': lng, 'name': name}
 
-			streets = line.split(',')
-			st1 = streets[1]
-			st2 = streets[2].split('\\')[0]
+		elif index >= startIndex and line[0]!='*' and 'END,,,,END' not in line:
 			lat, lng, name = 0, 0, ''
 
-			cKey = generateCacheKey(st1, st2)
 			if cKey in cache:
 				result = cache[cKey]
 				lat, lng, name = result['lat'], result['lng'], result['name']
@@ -52,21 +58,23 @@ def crawl():
 					print 'Url error %s: %s' %(err.code, err.read())
 				else:
 					results = json.load(response)['results'][0]
-					if results['types'][0]=='intersection':
+					if results['types'][0]=='intersection' and \
+						'San Francisco' in results['formatted_address']:
+
 						loc = results['geometry']['location']
 						lat, lng, name = loc['lat'], loc['lng'], results['formatted_address']
-						cache[cKey] = {'lat': lat, 'lng': lng, 'name': name}
 
+						cache[cKey] = {'lat': lat, 'lng': lng, 'name': name}
 						print 'success'
 		
 			# For each line in output file, 
 			# search success -> '* lat, long | formatted_address | original input'
 			# failure -> 'original input'
 			if lat > 0:
-				line = '* %f, %f | %s | ' %(loc['lat'], loc['lng'], results['formatted_address']) + line
+				line = '* %f, %f | %s | ' %(lat, lng, name) + line
 			else:
 				print 'failure'
-
+		
 		outputFile.write(line)
 	outputFile.close()
 
@@ -84,10 +92,11 @@ def getStat():
 
 # def validate():
 	# check lat, lng range
+	# Some results point to center of SF
 
 ###### Main ######
-# crawl()
-getStat()
+crawl()
+# getStat()
 	
 
 
