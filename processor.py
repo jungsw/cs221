f3decec57e443ffbcbd22391e055c75628390e68
jungsw <jungsw@stanford.edu>
2015-12-10 20:14:46 -0800

    shit fuck hooker

--- processor.py
+++ processor.py
@@ -30,21 +30,50 @@ class Processor(object):
     def processStreetName(self, inter):
         return inter.split('\\')[0]
     
+    def processNode(self, inter1, inter2):
+        node = internode.IntersectionNode(inter1, inter2, self.apikey)
+        return node
+    
     """
     Given an intersection as part of traffic_data, returns a list of adjacent streets
     based on the order of intersections in Intersections_only.csv
     """
-    def processNodeMap(self, inter1, inter2):
-        node = internode.IntersectionNode(inter1, inter2, self.apikey)
-        key = self.processStreetName(inter1) + ' | ' + self.processStreetName(inter2)
-        self.node_map[key] = node
+    def processTrafficData(self):
+        for index, row in self.traffic_data.iterrows():
+            adjacent_list = []
+            if self.traffic_data.at(index - 1, 'inter1') == row[0]:
+                key1 = self.traffic_data.at(index - 1, 'inter1') + ', ' + self.traffic_data.at(index - 1, 'inter2')
+                adjacent_list.append(key1)
+                print key1
+                
+            if self.traffic_data.at(index + 1, 'inter1') == row[0]:
+                key2 = self.traffic_data.at(index + 1, 'inter1') + ', ' + self.traffic_data.at(index + 1, 'inter2')
+                adjacent_list.append(key2)
+                print key2
+            
+            keylist = self.traffic_data[(self.traffic_data['inter1'] == row[1]) and (self.traffic_data['inter2'] == row[0])]
+            if len(keylist) == 1:
+                ind = keylist[0]    
+                if self.traffic_data.at(ind - 1, 'inter1') == row[0]:
+                    key3 = self.traffic_data.at(ind - 1, 'inter1') + ', ' + self.traffic_data.at(ind - 1, 'inter2')
+                    adjacent_list.append(key3)
+                    print key3
+                
+                if self.traffic_data.at(ind + 1, 'inter1') == row[0]:
+                    key4 = self.traffic_data.at(ind + 1, 'inter1') + ', ' + self.traffic_data.at(ind + 1, 'inter2')
+                    adjacent_list.append(key4)
+                    print key4
+                    print '--------------'
+            
+            node = row['node']
+            node.setAdjacents(adjacent_list)
     
     """
     Processes traffic intersection data and populates it with relevant information
     as a Pandas Data Frame
     """
     def processTrafficCSV(self, traffic_filename):
-        traffic_data = {'inter_1': [], 'inter_2': [], 'x_loc': [], 'y_loc': []}
+        traffic_data = {'inter1': [], 'inter2': [], 'x_loc': [], 'y_loc': [], 'node': []}
         with open(traffic_filename, 'rb') as csvfile:
             has_header = csv.Sniffer().has_header(csvfile.read(1024))
             csvfile.seek(0)            
@@ -54,12 +83,11 @@ class Processor(object):
                 next(mapreader)
                 
             for row in mapreader:
-                traffic_data['inter_1'].append(self.processStreetName(row[1]))
-                traffic_data['inter_2'].append(self.processStreetName(row[2]))
+                traffic_data['inter1'].append(self.processStreetName(row[1]))
+                traffic_data['inter2'].append(self.processStreetName(row[2]))
                 traffic_data['x_loc'].append(0.0)
                 traffic_data['y_loc'].append(0.0)
-                
-                self.processNodeMap(row[1], row[2])
+                traffic_data['node'].append(self.processNode(row[1], row[2]))
 
         self.traffic_data = pd.DataFrame(traffic_data)
         
@@ -101,6 +129,4 @@ nodemap = {'37.764861, -122.422886': ['37.764835, -122.423143', '37.764029, -122
 process = Processor('AIzaSyCufQQEadq3JZOx5sXfwpfy4AUcR1AIXMM')
 process.processTrafficCSV('List_of_Intersections_only.csv')
 process.processTrainCSV('../train-2.csv')
-print process.traffic_data
-print process.train_data
-print process.node_map
+process.processTrafficData()
